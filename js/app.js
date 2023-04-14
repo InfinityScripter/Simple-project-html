f01b6ae05748a61649992bbc748af3bbedc7d96f
kot <talalaev.msiah@gmail.com>
2023-04-14 16:41:21 +0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.burger').click(function(event) {
+        $('.header__link').toggleClass('show');
+    });
+
+    $('.header__link').click(function(event) {
+        $('.header__link').toggleClass('show');
+    });
+
+});
